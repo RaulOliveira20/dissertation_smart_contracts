f510532b50c39e8e14ece00173d2be55adada33d
RaulOliveira20 <36230040+RaulOliveira20@users.noreply.github.com>
2021-11-27 03:41:40 +0000

    Add files via upload

---
+++ FirstNPrimesBoth/FirstNPrimesBoth.py
@@ -0,0 +1,41 @@
+import smartpy as sp
+
+class FirstNPrimesBoth(sp.Contract):
+    def __init__(self):
+        self.init(last_prime = sp.int(0), m = {0 : 2})
+    
+    @sp.entry_point
+    def n_primes(self, num):
+        p = sp.local("p", 1)
+        i = sp.local("i", 3)
+        c = sp.local("c", 0)
+        n = sp.local("n", num)
+        
+        sp.while p.value < n.value:
+            
+            sp.while (i.value % self.data.m[c.value] != 0) & (self.data.m[c.value] * self.data.m[c.value] <= i.value):
+                c.value += 1
+            
+            sp.if (i.value % self.data.m[c.value] != 0):
+                self.data.m[p.value] = i.value
+                p.value += 1
+            
+            c.value = 0
+            
+            i.value += 2
+
+        self.data.m = {0 : 2}
+
+        sp.if (n.value <= 1):
+            self.data.last_prime = 2
+        sp.else:
+            self.data.last_prime = i.value - 2
+    
+@sp.add_test(name = "Testing getting the first N prime numbers")
+def test():
+    scenario = sp.test_scenario()
+
+    p = FirstNPrimesBoth()
+    scenario += p
+    
+    scenario += p.n_primes(1000)
