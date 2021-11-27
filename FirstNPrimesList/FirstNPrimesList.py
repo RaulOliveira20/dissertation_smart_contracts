0c1643edef068a5f7a37cb9dc6380c97b43631ba
RaulOliveira20 <36230040+RaulOliveira20@users.noreply.github.com>
2021-11-27 03:40:21 +0000

    Add files via upload

---
+++ FirstNPrimesList/FirstNPrimesList.py
@@ -0,0 +1,42 @@
+import smartpy as sp
+
+class FirstNPrimesList(sp.Contract):
+    def __init__(self):
+        self.init(last_prime = sp.int(0), primes = sp.list())
+    
+    @sp.entry_point
+    def n_primes(self, num):
+        self.data.primes = [2]
+        p = sp.local("p", 1)
+        i = sp.local("i", 3)
+        c = sp.local("c", 0)
+        n = sp.local("n", num)
+        
+        sp.while p.value < n.value:
+            
+            sp.for x in self.data.primes:
+                sp.if (i.value % x == 0):
+                    c.value = 1
+
+            sp.if (c.value == 0):
+                self.data.primes.push(i.value)
+                p.value += 1
+
+            c.value = 0       
+
+            i.value += 2
+        
+        self.data.primes = [2]
+        sp.if (n.value <= 1):
+            self.data.last_prime = 2
+        sp.else:
+            self.data.last_prime = i.value - 2
+    
+@sp.add_test(name = "Testing getting the first N prime numbers")
+def test():
+    scenario = sp.test_scenario()
+
+    p = FirstNPrimesList()
+    scenario += p
+    
+    scenario += p.n_primes(1000)
